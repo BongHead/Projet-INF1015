00d85c02cc0b2e2b116b734a7dda91860c3407d5
Bong <songning.lan@gmail.com>
2024-04-07 20:44:33 -0400

    Initial commit

---
+++ Projet1015/main.cpp
@@ -0,0 +1,12 @@
+#include "GameWindow.h"
+#include <QtWidgets/QApplication>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    GameWindow w;
+    w.show();
+    cout << "a";
+    return a.exec();
+}
